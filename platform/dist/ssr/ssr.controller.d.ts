bdf58c1f48f9a56a45ac9b8e027ce11302c5a0d2
darthnox123 <francisco__trindade@hotmail.com>
2026-06-01 15:54:00 +0100

    wip

--- platform/dist/ssr/ssr.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { SsrService } from './ssr.service';
-export declare class SsrController {
-    private readonly ssrService;
-    constructor(ssrService: SsrService);
-    render(req: Request, res: Response): Promise<void>;
-}
